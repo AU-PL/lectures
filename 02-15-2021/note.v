75cdd835f918dd9a0901e716826db3f33ebca550
Harley Eades <harley.eades@gmail.com>
2021-02-15 18:14:36 -0500

    Todays notes

--- 02-15-2021/note.v
+++ 02-15-2021/note.v
@@ -2,20 +2,35 @@ Inductive list (a : Type) : Type :=
 | empty
 | cons (x : a) (l : list a).
 
+Arguments empty {a}.
+Arguments cons {a}.
+
+Compute (cons 2 (cons 42 empty)).
+
+Definition l1 : list nat := cons 1 (cons 2 (cons 3 (cons 4 empty))).
+Definition l2 : list nat := cons 1
+                             (cons 2
+                                (cons 3
+                                   (cons 3
+                                      (cons 4
+                                         (cons 1
+                                            (cons 5 empty)))))).
+
+
 (* l3 := [1,2,3]  
  * f : nat -> nat       foo<a><b> 
  * f x := x + 3 
  * map f l3 = [f 1, f 2, f 3] 
  *          = [1 + 3, 2 + 3, 3 + 3] *)
-Fixpoint map (a b : Type) (f : a -> b) (l : list a) : list b :=
+Fixpoint map {a b : Type} (f : a -> b) (l : list a) : list b :=
   match l with
-  | empty _ => empty b
-  | cons _ x l' => cons b (f x) (map a b f l')
+  | empty => empty
+  | cons x l' => cons (f x) (map f l')
   end.
 
 Import Nat.
 
-Compute map nat nat (add 3) l1.
+Compute map (add 3) l1.
 
 (* 
  * lfold (reduce) : Left folds a list to a value.
@@ -25,17 +40,19 @@ Compute map nat nat (add 3) l1.
  * f auc x = 
  *
  * *)
-Fixpoint append (l1 l2 : natlist) : natlist :=
+Fixpoint append {a : Type} (l1 l2 : list a) : list a :=
   match l1 with
-  | empty _ => l2
-  | cons _ n l' => ncons n (append l' l2)
+  | empty => l2
+  | cons n l' => cons n (append l' l2)
   end.
 
+Definition id {a : Type} (x : a) : a := x.
+
 (* Not tail recursive  *)
-Fixpoint slow_reverse (l : natlist) : natlist :=
+Fixpoint slow_reverse (l : list nat) : list nat :=
   match l with
-  | empty _ => nempty
-  | cons _ n l' => append (slow_reverse l') (ncons n nempty)
+  | empty _ => empty
+  | cons _ n l' => append (slow_reverse l') (cons n empty)
   end.
 
 (* Accumulator Pattern : use "state"
@@ -45,24 +62,24 @@ Fixpoint slow_reverse (l : natlist) : natlist :=
  *
  * *)
 
-Fixpoint reverse' (acc : natlist) (l : natlist) : natlist :=
+Fixpoint reverse' (acc : list nat) (l : list nat) : list nat :=
   match l with
   | empty _ => acc
-  | cons _ n l' => reverse' (ncons n acc) l'
+  | cons _ n l' => reverse' (cons n acc) l'
   end.
 
-Compute (reverse' nempty l1).
+Compute (reverse' empty l1).
 
-Definition reverse (l : natlist) : natlist :=
-  reverse' nempty l.
+Definition reverse (l : list nat) : list nat :=
+  reverse' empty l.
 
-Lemma append_empty_left : forall l, append nempty l = l.
+Lemma append_empty_left : forall l, append empty l = l.
 Proof.
   intros l. 
   simpl.  reflexivity.
 Qed.
 
-Lemma append_empty_right : forall l, append l nempty = l.
+Lemma append_empty_right : forall l, append l empty = l.
 Proof.
   intro l.
   induction l as [_ | x l' IH].
