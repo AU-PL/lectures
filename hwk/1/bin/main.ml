8604efd8fece6636d951525c683d7a7e1eb49b8b
Harley D. Eades III <harley.eades@gmail.com>
2025-08-27 12:37:05 -0400

    Added homework 1.

---
+++ hwk/1/bin/main.ml
@@ -0,0 +1,18 @@
+(* 1. Define a variable using a let-expression called 'favMove' and set it to
+      your favorite movie as a string (string's in OCaml are written using
+      double-quotes.) *)
+
+
+(* 2. Define a function, called maxi, that takes in two integer parameters and
+       returns the max between them.  *)
+
+(* 3. Use `dune utop` to run the maxi function on three pairs of inputs. Copy
+      and paste your **complete** transcript here; this should include getting
+      utop to import this files contents.  *)
+
+(* Copy your transcript below this line, but inside the comment:
+ *
+ * 
+ *)
+
+ 
